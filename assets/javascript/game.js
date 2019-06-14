1e9c666f18399a38bd7edfe6010459367ebfd1ba
Shamera Boozer <snboozer@gmail.com>
2019-06-13 21:53:24 -0400

    code

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+
+
+  // var wins = 0;
+  // var losses = 0;
+  // var guesses = 0;
+  // var userGuess = []
+  
+  // var userGuessElement = document.getElementById("user-guesses");
+  // var computerGuessElement = document.getElementById("Computer-guess");
+  // console.log(userGuessElement)
+  // var WinsElement = document.getElementById("wins")
+  // var LossesElement = document.getElementById("losses")
+  // var guessesSoFarElement = document.getElementById("Guesses-So-Far");
+   
+  // for (i=1; i<=5; i++)
+  // {
+  // document.write(i)   
+  // }
+
+$('.crystal').on("click", function () {
+    console.log(this).getAttribute("value")
+    var value = $(this).attr("value")
+    console.log(obj[value])
+})
+
+  // var computerChoice = str[Math.floor(Math.random() * str.length)];
+  //   console.log(computerChoice)
+
+  //   if(userGuess === computerChoice){
+  //     wins++;
+  // document.getElementById("wins").innerText=wins
+  //     console.log(wins)
+  //     guessesNum = 9;
+  //     guessChoice = [];
+  // }
+  
+  // else {
+  //     losses++;
+// /      document.getElementById("losses").innerText=losses
