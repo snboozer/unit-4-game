0d25c1460b7fe557139617599c00c34e955f9cfc
Shamera Boozer <snboozer@gmail.com>
2019-06-15 09:57:47 -0400

    Code changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,77 @@
 
 
-  // var wins = 0;
-  // var losses = 0;
-  // var guesses = 0;
-  // var userGuess = []
-  
-  // var userGuessElement = document.getElementById("user-guesses");
-  // var computerGuessElement = document.getElementById("Computer-guess");
-  // console.log(userGuessElement)
-  // var WinsElement = document.getElementById("wins")
-  // var LossesElement = document.getElementById("losses")
-  // var guessesSoFarElement = document.getElementById("Guesses-So-Far");
-   
-  // for (i=1; i<=5; i++)
-  // {
-  // document.write(i)   
-  // }
+var wins = 0;
+var losses = 0;
+var computerChoice=0
+var score = document.getElementById("score");
 
-$('.crystal').on("click", function () {
-    console.log(this).getAttribute("value")
-    var value = $(this).attr("value")
-    console.log(obj[value])
+$('#buttonPurple').on("click", function () {
+    console.log("Purple");
+
 })
 
-  // var computerChoice = str[Math.floor(Math.random() * str.length)];
-  //   console.log(computerChoice)
 
-  //   if(userGuess === computerChoice){
-  //     wins++;
-  // document.getElementById("wins").innerText=wins
-  //     console.log(wins)
-  //     guessesNum = 9;
-  //     guessChoice = [];
-  // }
-  
+$('#buttonPink').on("click", function () {
+    console.log("Pink");
+})
+
+
+$('#buttonBlue').on("click", function () {
+    console.log("Blue");
+})
+
+$('#buttonRed').on("click", function () {
+    console.log("Red");
+})
+
+
+var winNum = Math.floor(Math.random() * Math.floor(100));
+$("#winNum").text(winNum);
+console.log(winNum)
+
+var winNum = Math.floor(Math.random() * Math.floor(100))
+$('#winNum').text(winNum);
+
+var buttonPurple = {
+    getValue: function () {
+        this.value = Math.floor(Math.random() * Math.floor(12));
+        console.log(this);
+        
+    }
+}
+var buttonPink = {
+    getValue: function () {
+        this.value = Math.floor(Math.random() * Math.floor(12));
+        console.log(this);
+    }
+}
+var buttonBlue = {
+    getValue: function () {
+        this.value = Math.floor(Math.random() * Math.floor(12));
+        console.log(this);
+    }
+}
+var buttonRed = {
+    getValue: function () {
+        this.value = Math.floor(Math.random() * Math.floor(12));
+        console.log(this);
+    }
+}
+
+buttonPurple.getValue();
+buttonPink.getValue();
+buttonBlue.getValue();
+buttonRed.getValue();
+
+// if (score === computerChoice) {
+    // wins++;
+    // document.getElementById("wins").innerText = wins
+    // console.log(wins)
+    // guessesNum = [];
+    // guessChoice = [];
+
+
+
   // else {
   //     losses++;
-// /      document.getElementById("losses").innerText=losses
+// /      document.getElementById("losses").innerText=losses}
